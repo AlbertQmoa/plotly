b9b830c053e86c1bcfe2c49e90e81604f2b0864a
AlbertQmoa <lovesphysics@gmail.com>
2024-02-15 20:51:03 +0800

    learn: pie chart; adding files

---
+++ pie_chart.py
@@ -0,0 +1,26 @@
+'''
+    A basic pie chart will sum the vlaues according to the names respectively 
+    and then show the percetage of each names.
+'''
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('data/bird-window-collision-death.csv')
+
+
+# ====== Step 1: use plotly.express to plot a sketch of your figure ====== 
+# https://plotly.com/python-api-reference/generated/plotly.express.pie.html#plotly.express.pie
+fig = px.pie(df, values='Deaths', names='Bldg #', color='Side', hole=0.3)
+
+
+# ====== Step 2: use plotly.graph_objective to refine your figure ======
+# Traces represent the data (inside the layout)
+# Layout represents the chart (frames, title, color, tick, hover, legend)
+fig.update_traces(textinfo='label+percent', insidetextfont={'color':'white'})
+fig.update_layout(legend={'itemclick':False})
+
+# ====== Step 3: Show and save your figure ======
+fig.show()
+
+fig.write_image('images/pie.png')
